d6f260ea2519c409da976743895518c078bd34ab
Adam Żmuda <adzik007@gmail.com>
2021-03-07 13:49:03 +0100

    available amount for budget and parent category

--- models/Budget.py
+++ models/Budget.py
@@ -1,16 +1,30 @@
-from .mydatabase import engine
+from database.mydatabase import engine
 
-from sqlalchemy import Column, Integer, String, ForeignKey, Numeric
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+from ParentCategory import ParentCategory
 
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 class Budget(Base):
     __tablename__ = 'budget'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    user_id = Column(Integer, ForeignKey("user.id")) #Creates relationship between the budget and the user
+    user_id = Column(Integer, ForeignKey("user.id"))
     parent_categories = relationship("ParentCategory")
+
+    def get_parent_categories(self):
+        return ParentCategory.query.filter((ParentCategory.budget_id == self.id)).all()
+
+    @property
+    def available_amount(self):
+        all_parent_categories = Budget.get_parent_categories()
+        available_amount = 0
+        for parent_category in all_parent_categories:
+            available_amount += parent_category.available_amount
+        return available_amount
--- models/ParentCategory.py
+++ models/ParentCategory.py
@@ -1,10 +1,14 @@
-from .mydatabase import engine
+from database.mydatabase import engine
 
 from sqlalchemy import Column, Integer, String, ForeignKey, Numeric
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+from Category import Category
+
 Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 class ParentCategory(Base):
@@ -14,3 +18,14 @@ class ParentCategory(Base):
     name = Column(String)
     budget_id = Column(Integer, ForeignKey("budget.id"))
     categories = relationship("Category")
+
+    def get_categories(self):
+        return ParentCategory.query.filter((ParentCategory.budget_id == self.id)).all()
+
+    @property
+    def available_amount(self):
+        all_categories = Category.get_categories()
+        available_amount = 0
+        for category in all_categories:
+            available_amount += category.available_amount
+        return available_amount
