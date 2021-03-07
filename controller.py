e26a6b9a0fc8657f25479a6a78758cb1229f342f
Daniel <easelpainting@gmail.com>
2021-03-07 17:49:18 +0100

    add comments

--- controller.py
+++ controller.py
@@ -8,12 +8,15 @@ from session import session
 global_user_id = None
 global_user_name = None
 
-#Define the logic
+#Define the logic here
 
+
+#Display login form
 def login():
     username = input("Hello, what's your name?\n")
     get_user(username)
 
+#Check if user exists, set global variables, redirect
 def get_user(username):
     user_instance = session.query(User).filter_by(name=username).first()
     if user_instance is None:
@@ -23,13 +26,16 @@ def get_user(username):
         global_user_name = user_instance.name
         show_budget(global_user_id, global_user_name)
 
+#Create account
 def create_account(username):
     print("\nHi, {}, seems like you don't have an account. Worry not! We've just created one for you! :-)".format(username))
     user = User(name=username)
     session.add(user)
     session.commit()
+    #set global variables
     get_user(username)
 
+#Show user's budget
 def show_budget(global_user_id, global_user_name):
     print("Hej, {}, oto Twój budżet!".format(global_user_name))
     #TODO
