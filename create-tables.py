c5ac247e5079e43fc86932f7d508b7b7dd4466e9
Daniel <easelpainting@gmail.com>
2021-03-07 15:57:56 +0100

    refactor 2

--- create-tables.py
+++ create-tables.py
@@ -1,18 +1,11 @@
 #Import database connection
 from database import engine
 
-# from sqlalchemy.ext.declarative import declarative_base
-# Base = declarative_base()
-
-#Import all models
-# from models import *
-
+#import models
 from models.Base import Base
 from models.User import User
 from models.Budget import Budget
 from models.ParentCategory import ParentCategory
-
-#set declarative class
 
 #Create the database
 Base.metadata.create_all(engine)
--- session.py
+++ session.py
@@ -1,6 +1,7 @@
+#import engine and sessionmaker
 from database import engine
 from sqlalchemy.orm import sessionmaker
 
+#create and export session
 Session = sessionmaker(bind=engine)
-
 session = Session()
