10db7edffe5991c4d4b07936a6ba7130c2a4e4fa
Adam Żmuda <adzik007@gmail.com>
2021-03-10 21:06:27 +0100

    added relationship

--- models/ParentCategory.py
+++ models/ParentCategory.py
@@ -19,6 +19,7 @@ class ParentCategory(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     budget_id = Column(Integer, ForeignKey("budget.id"))
+    categories = relationship("Category", backref="parent_category")
 
     def __repr__(self):
         return "ParentCategory '%s', wchodząca w skład budżetu o ID %i" % (self.name, self.budget_id)
