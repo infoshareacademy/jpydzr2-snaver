4f616e0657368e9712b32c739ac342f06d3f2f7b
Adam Żmuda <adzik007@gmail.com>
2021-03-15 19:32:33 +0100

    remove get_categories

--- models/ParentCategory.py
+++ models/ParentCategory.py
@@ -4,14 +4,6 @@ from sqlalchemy.orm import relationship
 from .Base import Base
 from .Category import Category
 
-# This is a hack to import session TODO fix this
-# https://stackoverflow.com/questions/30669474/beyond-top-level-package-error-in-relative-import
-import sys
-
-sys.path.append("..")
-
-from session import session
-
 
 class ParentCategory(Base):
     __tablename__ = 'parent_category'
@@ -23,6 +15,3 @@ class ParentCategory(Base):
 
     def __repr__(self):
         return "ParentCategory '%s', wchodząca w skład budżetu o ID %i" % (self.name, self.budget_id)
-
-    def get_categories(self):
-        return session.query(Category).filter(Category.parent_id == self.id).all()
