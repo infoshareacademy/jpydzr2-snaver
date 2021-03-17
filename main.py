9da246cb7473c47d6e7f8eec8e395b6f1e09d0b6
KatarzynaAhuja <katarzyna.meczkowska@tlen.pl>
2021-03-17 18:18:14 +0100

    Reading ASCII function added

--- main.py
+++ main.py
@@ -5,5 +5,13 @@ Induces controller.py
 
 from controllers.user_controller import login
 
+def reading_ascii(file_name):
+    with open(file_name, 'r') as file:
+        for line in file:
+            line = line.strip('\n')
+            print(line)
+
 print("\nWelcome to Snaver!")
 login()
+
+
