796ebffd598e2ea80c7e78dfc894e078718236a1
Adam Żmuda <adzik007@gmail.com>
2021-03-24 18:14:19 +0100

    add update_category_name function

---
+++ controllers/category_controller.py
@@ -0,0 +1,14 @@
+from session import session
+
+from models.Category import Category
+from models.Category import CategoryNotFoundException
+
+
+def update_category_name(category_id: int, new_name: str):
+    category_instance = session.query(Category).filter_by(id=category_id).first()
+    if category_instance:
+        category_instance.name = new_name
+        session.commit()
+        return category_instance
+    else:
+        raise CategoryNotFoundException(category_id)
